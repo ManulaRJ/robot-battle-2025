50a18ff5be0d95a6abc0d24e5dd4f1e6095cbdd6
ManulaRJ <166576603+ManulaRJ@users.noreply.github.com>
2025-10-03 12:23:06 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        "deep-blue": "#010026",
+        blue: "#010021",
+        red: "#DC4492",
+        yellow: "#22d3d6f7",
+        grey: "#ededed",
+        "dark-grey": "#757575",
+        "opaque-black": "rgba(0,0,0,0.35)",
+      },
+      backgroundImage: (theme) => ({
+        "gradient-rainbow":
+          "linear-gradient(81.66deg, #00B5EE 7.21%, #FF45A4 45.05%, #FFBA00 78.07%)",
+
+        "gradient-rainblue":
+          "linear-gradient(90deg, #2582B5 14.53%, #0098FF 69.36%, #00366E 117.73%)",
+          "circularLight":'repeating-radial-gradient(rgba(255,255,255,0.4)2px,#010026 5px,#010026 100px)',
+      }),
+      fontFamily: {
+        playfair: ["russo one", "sans-serif"],
+        opensans: ["Poppins", "sans-serif"],
+      },
+      content: {
+        brush: "url('./assets/brush.png')",
+        person1: "url('./assets/person-1.png')",
+        person2: "url('./assets/person-2.png')",
+        person3: "url('./assets/person-3.png')",
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
